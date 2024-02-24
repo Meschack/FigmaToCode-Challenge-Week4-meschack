bf8f7deaadb80c0e3f144d2a6cee95baa31dca3f
Meschack <meshachgbewezoun@gmail.com>
2024-02-24 18:39:37 +0100

    Save

--- src/components/organisms/my-testimonial.tsx
+++ src/components/organisms/my-testimonial.tsx
@@ -34,13 +34,16 @@ export const MyTestimonial = () => {
     <div>
       <div className='max-w-7xl mx-auto'>
         <div className='mx-auto w-full max-w-[1216px] py-10'>
-          <h2 className='w-fit mx-auto'>
+          <h2 className='w-fit mx-auto p-0 text-3xl'>
             <span className='font-normal'>My</span> Testimonial
           </h2>
 
           <div className='grid grid-cols-3 gap-6 justify-between'>
             {review.map((review, index) => (
-              <div key={index} className='rounded-md shadow-md min-w-[370px] flex items-center flex-col gap-5 p-5'>
+              <div
+                key={index}
+                className='rounded-md shadow-md min-w-[370px] flex  hover:bg-black cursor-pointer hover:text-white items-center flex-col gap-5 p-5'
+              >
                 <Image
                   src={review.picture}
                   className='rounded-full'
@@ -49,7 +52,7 @@ export const MyTestimonial = () => {
                   width={96}
                 />
 
-                <p className='text-center'>{review.message}</p>
+                <p className='text-center min-w-[290px]'>{review.message}</p>
 
                 <div className='divide-white'></div>
 
